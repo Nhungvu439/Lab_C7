bfeb46f81cab6d328a41e97ec2772ad0bd3002b6
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2026-03-11 00:47:55 +0700

    Lab C7

---
+++ Assets/MenuTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuTest : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Open Options");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game");
+    }
+}
